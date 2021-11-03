f2e2f55824440651a05eec750ce27ecd0187d2bb
natalie-coder <63603124+natalie-coder@users.noreply.github.com>
2021-11-03 21:17:19 +0000

    Set up extends class for Defence ship

--- hostile-aliens/main.js
+++ hostile-aliens/main.js
@@ -5,13 +5,30 @@ const buttonGame = document.getElementById("button");
 function buttonClicked () {
     buttonGame.addEventListener("click",(event) => {
         let randomNumber = ((Math.floor(Math.random() * 14)) + 1);
-        if (randomNumber === 1) {
+        if (randomNumber === 14) {
             console.log("Mothership");
             console.log(motherShip.reduceScore());
-            } else if (randomNumber === 2 || randomNumber === 3 || randomNumber === 4 || randomNumber === 5 || randomNumber === 6) {
-                console.log("Defence ship");
-                console.log(defenceShip.reduceScore());
-            } else {
+        } else if (randomNumber === 1) { 
+            console.log("Defence ship 1");
+            console.log(defenceShip1.reduceScore());
+            console.log(defenceShip1.shipDestroyed());
+        } else if (randomNumber === 2) {
+            console.log("Defence ship 2");
+            console.log(defenceShip2.reduceScore());
+            console.log(defenceShip2.shipDestroyed());
+        } else if (randomNumber === 3) {
+            console.log("Defence ship 3");
+            console.log(defenceShip3.reduceScore());
+            console.log(defenceShip3.shipDestroyed());
+        } else if (randomNumber === 4) {
+            console.log("Defence ship 4");
+            console.log(defenceShip4.reduceScore());
+            console.log(defenceShip4.shipDestroyed());
+        } else if (randomNumber === 5) {
+            console.log("Defence ship 5");
+            console.log(defenceShip5.reduceScore());
+            console.log(defenceShip5.shipDestroyed());
+        } else {
                 console.log("Attack ship");
                 console.log(attackShip.reduceScore());
             }
@@ -20,8 +37,7 @@ function buttonClicked () {
 }
 
 class Alien {
-    constructor(name, score, minusPoints) {
-        this.name = name;
+    constructor(score, minusPoints) {
         this.score = score;
         this.minusPoints = minusPoints;
     }
@@ -29,11 +45,33 @@ class Alien {
         return (this.score -= this.minusPoints)
         
     }
+    
+    
 }
 
-const motherShip = new Alien ("Mothership", 100, 9);
-const defenceShip = new Alien ("DefenceShip", 80, 10);
-const attackShip = new Alien ("AttackShip", 45, 12);
+class DefenceShip extends Alien {
+    constructor(score, minusPoints, name) {
+        super(score, minusPoints) 
+            this.name = name; 
+    }
+    shipDestroyed() {
+        if(this.score <= 0) {
+            console.log("SHIP DESTROYED");
+
+        
+    }
+}
+}
+
+const motherShip = new Alien ( 100, 9);
+const attackShip = new Alien ( 45, 12);
+
+
+const defenceShip1 = new DefenceShip (80, 10, "defenceShip1");
+const defenceShip2 = new DefenceShip (80, 10, "defenceShip2");
+const defenceShip3 = new DefenceShip (80, 10, "defenceShip3");
+const defenceShip4 = new DefenceShip (80, 10, "defenceShip4");
+const defenceShip5 = new DefenceShip (80, 10, "defenceShip5");
 
 
 
@@ -42,23 +80,5 @@ const attackShip = new Alien ("AttackShip", 45, 12);
 
 
 
-
-
-
-// function alienName () {
-// if (randomNumber === 5) {
-// console.log("Mothership");
-// } else {
-//     console.log(randomNumber);
-
-// }
-// }
-
-// let clickedLetter;
-// function letterClicked () {
-//     for (var i = 0; i < squareLetter.length; i++) {
-//         squareLetter[i].addEventListener("click", (event) => {
-//     clickedLetter = event.target.value;
-//    correctLetter();
 
 buttonClicked();
